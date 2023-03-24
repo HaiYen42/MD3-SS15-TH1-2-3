f58073ac7f3d977423d2ea2f2c9a57b094f7efe9
HaiYen42 <haiyentth42@gmail.com>
2023-03-24 16:24:01 +0700

    TH123

---
+++ src/TH3CalculationExample.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TH3CalculationExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hãy nhập x: ");
+        int x = scanner.nextInt();
+        System.out.println("Hãy nhập y: ");
+        int y = scanner.nextInt();
+        CalculationExample calc = new CalculationExample();
+        calc.calculate(x, y);
+    }
+
+}
